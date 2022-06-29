cd22ff0f8d5a3e55fd00097fbb9cf124748f5beb
Eriberto Contreras <eriberto.contreras91@gmail.com>
2022-06-29 09:45:25 -0500

    exercises

--- prepare.py
+++ prepare.py
@@ -17,21 +17,23 @@ import requests
 
 def prep_store_data(df):
     df.sale_date = df.sale_date.apply(lambda date: date[:-13])
-    df.sale_date = pd.to_datetime(df.sale date, format= '%a, %d %b %y')
+    df.sale_date = pd.to_datetime(df.sale_date, format='%a, %d %b %Y')
     # make sure we sort by date/time before resampling or doing other time series manipulations
     df = df.set_index('sale_date').sort_index()
-    df = df.rename(columns={'sale_amount': 'quantity'})
     df['month'] = df.index.strftime('%m-%b')
-    df['dow'] = df.index.strftime('%w-%a')
-    df['sales_total'] = df. quantity * df.item_price
+    df['day_of_week'] = df.index.strftime('%w-%a')
+    df['sales_total'] = df.sale_amount * df.item_price
     return df
 
 def prep_opsd_data(df):
-    df.columns = [column.replace('+','_').lower() for column in df]
+    #renaming collumns and making then lower case.
+    df.columns = ['date', 'consumption', 'wind', 'solar', 'wind_solar']
     df.date = pd.to_datetime(df.date)
-    df = df.set_index('date') .sort_index()
+    df = df.set_index('date').sort_index()
     df['month'] = df.index.strftime('%m-%b')
     df['year'] = df.index.year
     df = df.fillna(0)
-    df['wind solar'] = df.wind + df.solar
+    df['wind_solar'] = df.wind + df.solar
     return df
+
+
